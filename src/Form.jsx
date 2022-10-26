40718f8772a896650753295eb06577395cdee7db
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-26 21:29:38 +0530

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className=" bg-gray-500 p-4">
+      <form>
+        <label>Title</label>
+        <input className=" border border-gray-500" />
+      </form>
+    </div>
+  );
+};
+export default Form;
